2572ef2e2970ea2b8102764f8dc09de5b57ff9ce
carlos vinícius ferrareze <viniferrareze@gmail.com>
2019-10-07 23:22:56 -0300

    adicionado real time

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,11 +1,24 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Image, AsyncStorage, ScrollView} from 'react-native';
+import {SafeAreaView, StyleSheet, Alert, Image, AsyncStorage, ScrollView} from 'react-native';
+import socketio from 'socket.io-client';
 
 import logo from '../assets/logo.png';
 import SpotList from '../components/SpotList';
 
 export default function List(){
    const [techs, setTechs] = useState([]);
+
+   useEffect(() => {
+      AsyncStorage.getItem('user').then(user_id => {
+         const socket = socketio('http://192.168.1.15:3333', {
+            query: {user_id}
+         });
+
+         socket.on('booking_response', booking => {
+            Alert.alert(`Sua Reserva em ${booking.spot.company} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'} `)
+         });
+      });
+   }, []);
 
    useEffect(() => {
       AsyncStorage.getItem('techs').then(storageTech => {
